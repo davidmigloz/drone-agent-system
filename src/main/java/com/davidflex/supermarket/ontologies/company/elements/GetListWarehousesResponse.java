4b3ae80b131beea78bcb73b020f12a7333e2e0d5
geekymoose <constantin.grinda@gmail.com>
2016-06-09 19:19:50 +0200

    Add GetListWarehouses

---
+++ src/main/java/com/davidflex/supermarket/ontologies/company/elements/GetListWarehousesResponse.java
@@ -0,0 +1,28 @@
+package com.davidflex.supermarket.ontologies.company.elements;
+
+import jade.content.Predicate;
+
+import java.util.List;
+
+/**
+ * Response with warehouses list.
+ */
+@SuppressWarnings("unused")
+public class GetListWarehousesResponse implements Predicate{
+    private List<Warehouse> warehouses;
+
+    public GetListWarehousesResponse() {
+    }
+
+    public GetListWarehousesResponse(List<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+
+    public List<Warehouse> getWarehouses() {
+        return warehouses;
+    }
+
+    public void setWarehouses(List<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+}
