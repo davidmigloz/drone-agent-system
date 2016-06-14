d85ca79201e0fb18ab84494d5e1d6178828fecf4
davidmigloz <me@davidmiguel.com>
2016-06-14 20:52:45 +0200

    Implement HandleDeliverBehavior of DroneAgent

--- src/main/java/com/davidflex/supermarket/ontologies/ecommerce/predicates/DeliverRequest.java
+++ src/main/java/com/davidflex/supermarket/ontologies/ecommerce/predicates/DeliverRequest.java
@@ -1,13 +1,14 @@
 package com.davidflex.supermarket.ontologies.ecommerce.predicates;
 
 import com.davidflex.supermarket.ontologies.ecommerce.concepts.Item;
+import jade.content.Predicate;
 
 import java.util.List;
 
 /**
  * Drones PersonalAgent that it is ready for delivering the order.
  */
-public class DeliverRequest {
+public class DeliverRequest implements Predicate{
 
     private List<Item> items;
 
