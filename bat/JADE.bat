b479698b491c60a5e10b98b605f30e541f0b94d1
davidmigloz <me@davidmiguel.com>
2016-06-16 18:47:13 +0200

    Add screenshot

--- bat/JADE.bat
+++ bat/JADE.bat
@@ -1,3 +1,3 @@
 SET port=1099
 
-java -cp ./target/Supermarket-0.0.1-SNAPSHOT.jar jade.Boot -gui -local-port %port%
+java -cp ./target/Supermarket-0.0.1-SNAPSHOT.jar jade.Boot -gui -local-host 192.168.43.100 -local-port %port%
--- bat/ShopGUI.bat
+++ bat/ShopGUI.bat
@@ -1,4 +1,4 @@
-SET host=192.168.229.1
+SET host=192.168.43.100
 SET port=1099
 
-java -cp ./target/Supermarket-0.0.1-SNAPSHOT.jar jade.Boot -container -container-name shopGUI shopGUI:com.davidflex.supermarket.agents.shop.ShopAgentGuiAgent -host %host% -port %port%
+java -cp ./target/Supermarket-0.0.1-SNAPSHOT.jar jade.Boot -container -container-name shopGUI -host %host% -port %port% shopGUI:com.davidflex.supermarket.agents.shop.ShopAgentGuiAgent
